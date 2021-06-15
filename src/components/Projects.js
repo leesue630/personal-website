d20492b751c6783d1b407bfdb892976c084c46c7
leesue630 <lee.sue630@gmail.com>
2021-06-15 00:37:03 -0400

    capitalize www

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -58,7 +58,7 @@ function Projects() {
       time: "Summer 2020",
       image: personal_website,
       description:
-        "💻 This website, which uses React + Material UI to share who I am with the www!",
+        "💻 This website, which uses React + Material UI to share who I am with the WWW!",
       website: "http://suelee.netlify.app",
       github: "https://github.com/leesue630/personal-website",
     },
